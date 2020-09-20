206183ac54b1f9c906a81c2ccc0accfd55dc932c
prakrutivaghasiya <prakrutivaghasiya@gecg28.ac.in>
2020-09-20 22:09:02 +0530

    body card part complete

--- ui-dribbble/src/components/Body.js
+++ ui-dribbble/src/components/Body.js
@@ -6,69 +6,78 @@ const Body = () => {
     {
       id: 1,
       name: 'Apple Inc.',
-      logo: '',
+      logo: 'http://pngimg.com/uploads/apple_logo/apple_logo_PNG19690.png',
       time: '02d:15h:54m',
       description: 'iOS 10 Lead Team Design',
       teams: 89,
       budget: '$88.5m',
       success: '15.2%',
+      color: true,
     },
     {
       id: 2,
-      name: 'Nike Inc.',
-      logo: '',
+      name: 'Puma Inc.',
+      logo:
+        'https://www.pinclipart.com/picdir/middle/537-5375013_puma-logo-png-transparent-images-puma-logo-transparent.png',
       time: '10d:21h:34m',
       description: 'Fall Winter 2016 commercial',
       teams: 109,
       budget: '$14.5m',
       success: '5.9%',
+      color: true,
     },
     {
       id: 3,
-      name: 'Samsung Electronics Co. Ltd',
-      logo: '',
+      name: 'linkedIn',
+      logo: 'http://pngimg.com/uploads/linkedIn/linkedIn_PNG32.png',
       time: '23d:05h:17m',
-      description: 'Samsung Center App Design',
+      description: 'linkedIn Job Portal',
       teams: 642,
       budget: '$10.5m',
       success: '28.2%',
+      color: true,
     },
     {
       id: 4,
       name: 'Evenote Corp.',
-      logo: '',
+      logo: 'https://www.iconspng.com/uploads/evernote-elephant-icon-logo.png',
       time: '02m:20d:15h',
       description: 'Full Product Design',
       teams: 856,
       budget: '$8.7m',
       success: '67.4%',
+      color: false,
     },
     {
       id: 5,
       name: 'Twitter Inc.',
-      logo: '',
+      logo: 'http://pngimg.com/uploads/twitter/twitter_PNG34.png',
       time: '02m14d:6h',
       description: 'Mobile Clients Design',
       teams: 964,
       budget: '$5m',
       success: '10.3%',
+      color: false,
     },
     {
       id: 6,
-      name: 'SoundCloud LLC.',
-      logo: '',
+      name: 'YouTube LLC.',
+      logo: 'http://pngimg.com/uploads/youtube/youtube_PNG19.png',
       time: '03m:10d:22h',
       description: 'Innovative Product Monetization',
       teams: 230,
       budget: '$4.9m',
       success: '39.2%',
+      color: false,
     },
   ];
   return (
     <div className='body'>
-      {data.map((content) => (
-        <Card key={content.id} content={content} />
-      ))}
+      <div className='card-section'>
+        {data.map((content) => (
+          <Card key={content.id} content={content} />
+        ))}
+      </div>
     </div>
   );
 };
--- ui-dribbble/src/components/Card.js
+++ ui-dribbble/src/components/Card.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
-const Card = ({ content }) => {
-  return <div className='card'>{content.id}</div>;
+const Card = ({
+  content: { name, logo, time, description, teams, budget, success, color },
+}) => {
+  return (
+    <div className='card'>
+      <div className='name'>
+        <strong>{name}</strong>
+      </div>
+      <div className='image'>
+        <img src={logo} alt='company-logo' />
+      </div>
+      <div className={`time ${color ? 'orange' : ''}`}>{time}</div>
+      <h5 className='desc'>{description}</h5>
+      <div className='info'>
+        <div className='teams'>
+          <h5>{teams}</h5>
+          <p className='text-medium'>teams</p>
+        </div>
+        <div className='budget'>
+          <h5>{budget}</h5>
+          <p className='text-medium'>budget</p>
+        </div>
+        <div className='success'>
+          <h5>{success}</h5>
+          <p className='text-medium'>success</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
